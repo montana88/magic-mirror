6ac812ffbf99f62ebb6f90efad0c9749ad9c8987
Toni <tmaat88@gmail.com>
2015-09-15 14:57:19 +0200

    add all files to git

--- index.php
+++ index.php
@@ -9,19 +9,25 @@
 <body>
 
     <div id="clock" class="bottom center-hor">
-        <div id="time"></div>
-        <div id="message"></div>
+        <div id="time" class="small"></div>
+        <div id="message" class="small"></div>
     </div>
 
-    <div id="weather" class="right top"></div>
+    <div class="bottom right"><div class="date small dimmed"></div><div class="time"></div><div class="calendar xxsmall"></div></div>
 
-    <div id="news" class="left top"></div>
+    <div id="weather" class="right small top"></div>
+
+    <div id="news" class="left xxsmall top"></div>
 
     <script src="js/jquery-2.1.4.min.js"></script>
     <script src="js/clock.js"></script>
     <script src="js/cheeringMessage.js"></script>
+    <script src="js/rrule.js"></script>
+    <script src="js/socket.io.min.js"></script>
+    <script src="js/moment-with-langs.min.js"></script>
     <script src="js/weather.js"></script>
     <script src="js/newsFeed.js"></script>
+    <script src="js/ical_parser.js"></script>
     <script src="js/main.js"></script>
 
 </body>
--- js/main.js
+++ js/main.js
@@ -164,16 +164,139 @@ var news = {
 
 };
 
+var calendar = {
+
+    eventList: [],
+
+    updateCalendarData: function() {
+
+        var that = this;
+
+        new ical_parser("calendar.php", function(cal){
+            var events = cal.getEvents();
+            that.eventList = [];
+
+            for (var i in events) {
+                var e = events[i];
+                for (var key in e) {
+                    var value = e[key];
+                    var seperator = key.search(';');
+                    if (seperator >= 0) {
+                        var mainKey = key.substring(0,seperator);
+                        var subKey = key.substring(seperator+1);
+
+                        var dt;
+                        if (subKey == 'VALUE=DATE') {
+                            //date
+                            dt = new Date(value.substring(0,4), value.substring(4,6) - 1, value.substring(6,8));
+                        } else {
+                            //time
+                            dt = new Date(value.substring(0,4), value.substring(4,6) - 1, value.substring(6,8), value.substring(9,11), value.substring(11,13), value.substring(13,15));
+                        }
+
+                        if (mainKey == 'DTSTART') e.startDate = dt;
+                        if (mainKey == 'DTEND') e.endDate = dt;
+                    }
+                }
+
+                if (e.startDate == undefined){
+                    //some old events in Gmail Calendar is "start_date"
+                    //FIXME: problems with Gmail's TimeZone
+                    var days = moment(e.DTSTART).diff(moment(), 'days');
+                    var seconds = moment(e.DTSTART).diff(moment(), 'seconds');
+                    var startDate = moment(e.DTSTART);
+                } else {
+                    var days = moment(e.startDate).diff(moment(), 'days');
+                    var seconds = moment(e.startDate).diff(moment(), 'seconds');
+                    var startDate = moment(e.startDate);
+                }
+
+                //only add fututre events, days doesn't work, we need to check seconds
+                if (seconds >= 0) {
+                    if (seconds <= 60*60*5 || seconds >= 60*60*24*2) {
+                        var time_string = moment(startDate).fromNow();
+                    }else {
+                        var time_string = moment(startDate).calendar()
+                    }
+                    if (!e.RRULE) {
+                        that.eventList.push({'description':e.SUMMARY,'seconds':seconds,'days':time_string});
+                    }
+                    e.seconds = seconds;
+                }
+
+                // Special handling for rrule events
+                if (e.RRULE) {
+                    var options = new RRule.parseString(e.RRULE);
+                    options.dtstart = e.startDate;
+                    var rule = new RRule(options);
+
+                    // TODO: don't use fixed end date here, use something like now() + 1 year
+                    var dates = rule.between(new Date(), new Date(2016,11,31), true, function (date, i){return i < 10});
+                    for (date in dates) {
+                        var dt = new Date(dates[date]);
+                        var days = moment(dt).diff(moment(), 'days');
+                        var seconds = moment(dt).diff(moment(), 'seconds');
+                        var startDate = moment(dt);
+                        if (seconds >= 0) {
+                            if (seconds <= 60*60*5 || seconds >= 60*60*24*2) {
+                                var time_string = moment(dt).fromNow();
+                            } else {
+                                var time_string = moment(dt).calendar()
+                            }
+                            that.eventList.push({'description':e.SUMMARY,'seconds':seconds,'days':time_string});
+                        }
+                    }
+                }
+            };
+
+            that.eventList.sort(function(a,b){return a.seconds-b.seconds});
+
+            setTimeout(function() {
+                that.updateCalendarData();
+            }, 10000);
+        });
+    },
+
+    updateCalendar: function() {
+
+        var table = $('<table/>').addClass('xsmall').addClass('calendar-table');
+        var opacity = 1;
+        var that = this;
+
+        for (var i in that.eventList) {
+            var e = that.eventList[i];
+
+            var row = $('<tr/>').css('opacity',opacity);
+            row.append($('<td/>').html(e.description).addClass('description'));
+            row.append($('<td/>').html(e.days).addClass('days dimmed'));
+            table.append(row);
+
+            opacity -= 1 / that.eventList.length;
+        }
+
+        $('.calendar').html(table);
+
+        setTimeout(function() {
+            that.updateCalendar();
+        }, 1000);
+    }
+
+}
+
 if(document.readyState === "complete") {
     item1.clock();
     item1.compliment();
     currentWeather.init();
     news.init();
+    calendar.updateCalendarData();
+    calendar.updateCalendar();
 } else {
     window.addEventListener("DOMContentLoaded", function () {
         item1.clock();
         item1.compliment();
         currentWeather.init();
         news.init();
+        calendar.updateCalendarData();
+        calendar.updateCalendar();
     }, false);
 }
